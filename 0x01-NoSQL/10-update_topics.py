914160851d0a99ae8db4b18692889af1f1d81d93
R-Owino <sayeakinyi@gmail.com>
2023-10-20 15:09:46 -0700

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Contains a function update_topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school document based on the school's name
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The PyMongo
+        collection object
+        name (str): The school name to update
+        topics (list): The list of topics to be set for the school
+
+    Returns:
+        int: The number of documents updated (0 or 1)
+    """
+    filter_criteria = {"name": name}
+    update_data = {"$set": {"topics": topics}}
+    result = mongo_collection.update_one(filter_criteria, update_data)
+    return result.modified_count
